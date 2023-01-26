7239471334bb0cf573072290c37e82c6acb30c04
rahul4code <you@example.com>
2023-01-27 01:06:24 +0530

    chapter 2: shot 1

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading1 = React.createElement("h1", {}, "Hi Rahul");
 const heading2=React.createElement("h2", {id:"test", style:{background:"Yellow"}}, "Be productive")
 const container=React.createElement("div", {}, [heading1,heading2])
